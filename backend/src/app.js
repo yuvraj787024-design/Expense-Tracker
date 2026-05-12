4870849378933d61081b287c097397375e98f1c4
Yuvraj Kumar <yuvraj787024@gmail.com>
2026-05-12 23:52:02 +0530

    again fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,11 +11,7 @@ app.use(cookieParser())
 
 
 app.use(cors({
-    origin: [
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "https://expense-tracker-7l5v.vercel.app"
-    ],
+    origin: true,
     credentials: true
 }))
 
